70bc7a9598e4db2c7ef681fbe690956a759d5aad
MathGuiMach <mathguimach@gmail.com>
2020-12-01 10:01:29 -0300

    Exception handling - Delete

--- src/main/java/com/mathguimach/demo_springboot/services/UserService.java
+++ src/main/java/com/mathguimach/demo_springboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mathguimach.demo_springboot.entities.User;
 import com.mathguimach.demo_springboot.repositories.UserRepository;
+import com.mathguimach.demo_springboot.services.exceptions.DatabaseException;
 import com.mathguimach.demo_springboot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		ur.deleteById(id);
+		try {			
+			ur.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
