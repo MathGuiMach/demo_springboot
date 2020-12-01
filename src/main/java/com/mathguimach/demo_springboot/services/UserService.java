a691f7c5f731bd332e56f7110ca67dc57294a270
MathGuiMach <mathguimach@gmail.com>
2020-12-01 10:05:08 -0300

    Exception Handling - Update

--- src/main/java/com/mathguimach/demo_springboot/services/UserService.java
+++ src/main/java/com/mathguimach/demo_springboot/services/UserService.java
@@ -3,6 +3,8 @@ package com.mathguimach.demo_springboot.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = ur.getOne(id);
-		updateData(entity, obj);
-		return ur.save(entity);
+		try {
+			User entity = ur.getOne(id);
+			updateData(entity, obj);
+			return ur.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
