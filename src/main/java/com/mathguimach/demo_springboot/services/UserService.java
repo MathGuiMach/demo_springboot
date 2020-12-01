794fc6b13188f8616df0fffcb2ec7ec9e8682663
MathGuiMach <mathguimach@gmail.com>
2020-12-01 09:51:01 -0300

    Exception Handling - findById

--- src/main/java/com/mathguimach/demo_springboot/services/UserService.java
+++ src/main/java/com/mathguimach/demo_springboot/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.mathguimach.demo_springboot.entities.User;
 import com.mathguimach.demo_springboot.repositories.UserRepository;
+import com.mathguimach.demo_springboot.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj =  ur.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
